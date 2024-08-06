b210ee1ea66ac61ce6a2a1763c0d304e6a7bb389
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-08-06 10:54:05 +0300

    Adding taskRoutes to routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { taskRoutes } from './routes/TaskRoutes';
 
 export const routes = Router();
 
 // All of the routes from different route files go here
+routes.use(taskRoutes);

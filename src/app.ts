ebced8e7a2e04ef6efcc87661f560cfb1657ec89
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-08-08 11:09:14 +0300

    Fixing routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import { TaskModel } from "./models/Task";
+import { routes } from "./routes";
 export const app: Express = express();
+
+app.use(routes);
 
 // An example route for testing if the server works
 app.get('/example', (req: Request, res: Response) => {
--- src/routes.ts
+++ src/routes.ts
@@ -4,4 +4,4 @@ import { taskRoutes } from './routes/TaskRoutes';
 export const routes = Router();
 
 // All of the routes from different route files go here
-routes.use(taskRoutes);
+routes.use("/tasks", taskRoutes);

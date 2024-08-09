eb77aede229f7f2c2f0dffd31cb87e0b8b339aa5
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-08-09 10:36:32 +0300

    Adding endpoint for creating task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import { Task, TaskModel  } from "../models/Task";
 import {TaskService} from "../services/TaskService";
+import { Error } from "mongoose";
+import { formatErrors } from "../database/formatErrors";
 
 let taskService = new TaskService();
 
 export const createTask = async (req: Request, res: Response) => {
+    let errors: {[key: string]: string} | null = null;
     const requestBody = await req.body;
-    await taskService.createTask(requestBody);
+    const taskRes: Error.ValidationError | Task | null = await taskService.createTask(requestBody);
+    if(taskRes) {
+        if (taskRes instanceof Error.ValidationError) {
+            errors = formatErrors(taskRes);
+        }
 
-    res.status(200).json({"Message":"Task created!"});
+        if(errors) {
+            res.status(400).json(errors);
+        } else {
+            res.status(200).json({"Message":"Task created!"});
+        }
+    }
+    res.status(500);
 }
 
 export const getTasks = async (req: Request, res: Response) => {

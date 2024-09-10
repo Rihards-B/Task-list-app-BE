bc2cfe6c7110993ef70599e1a8fe0b63f6877880
Rihards Blekus <rihardsblekus2002+01@gmail.com>
2024-09-10 11:58:52 +0300

    Adding credentials to cors options

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, json, Request, Response } from "express";
 import { TaskModel } from "./models/Task";
 import { routes } from "./routes";
+import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 export const app: Express = express();
 
-const allowedOrigins = ['http://localhost:3131', 'http://localhost:4200'];
+dotenv.config();
 
+const allowedOrigins = ['http://localhost:4200', 'http://localhost:3131'];
 const options: cors.CorsOptions = {
-    origin: allowedOrigins
+    origin: allowedOrigins,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 };
 
-app.use(cors());
+app.use(json());
+app.use(cors(options));
+app.use(cookieParser());
 app.use(routes);
 
 // An example route for testing if the server works

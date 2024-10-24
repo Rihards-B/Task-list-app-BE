4f56ad69f7d0a8ae52f26f82721bb56fc4aaebc2
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-24 13:44:37 +0300

    Removing _id from roles and checking by name which is now also unique

--- src/middleware/auth/validateToken.ts
+++ src/middleware/auth/validateToken.ts
@@ -44,12 +44,8 @@ export const validateToken = (roleName?: string) => {
 }
 
 async function checkRole(user: User, roleName: string): Promise<boolean> {
-  const roles = await RoleModel.find();
-  const adminRole = roles.find(role => role.roleName === roleName);
-  if (user && adminRole) {
-    if (user.roles.find(role => role._id == adminRole._id)) {
-      return true;
-    }
+  if (user.roles.find(role => role.roleName == "Admin")) {
+    return true;
   }
   return false
 }
--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,12 +1,11 @@
 import { model, Schema } from "mongoose";
 
 export interface Role {
-    _id?: string,
     roleName: string
 }
 
 const roleSchema = new Schema<Role>({
-    roleName: { type: String, required: true },
+    roleName: { type: String, required: true, unique: true },
 });
 
 export const RoleModel = model<Role>('Role', roleSchema);

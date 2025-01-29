1af53dce4f51aebc4d7edbf5ffd065f66e6bd72f
Rihards-Blekus <rihards.blekus@vasscompany.com>
2025-01-29 15:52:48 +0200

    Reverting back to using an error response code 401

--- src/responses/AuthResponses.ts
+++ src/responses/AuthResponses.ts
@@ -8,7 +8,7 @@ export namespace AuthResponses {
     }
 
     export const InvalidCredentials = (response: Response) => {
-        response.status(200).json({ "isLoggedIn": false, "user": null, "errors": ["Invalid Credentials"] });
+        response.status(401).json({ "messages": "Invalid credentials" });
     }
 
     export const NotAuthorized = (response: Response) => {

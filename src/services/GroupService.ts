bbd17af7aa8169418fe8344c5dfbe642f1f0d67d
Rihards-Blekus <rihards.blekus@vasscompany.com>
2025-02-06 14:21:24 +0200

    Adding a function to check if a group exists

--- src/services/GroupService.ts
+++ src/services/GroupService.ts
@@ -4,4 +4,8 @@ export class GroupService {
     public async createGroup(groupName: string) {
         return await GroupModel.create({ name: groupName });
     }
+
+    public async checkIfGroupExists(groupName: string) {
+        return await GroupModel.find({ name: groupName }) ? true : false
+    }
 }
